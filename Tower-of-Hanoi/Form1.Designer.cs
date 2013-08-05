cf93c8d042cc47b7b41312b1b6e8f77054ef439c
flsandman <samebarlow@gmail.com>
2013-08-05 12:00:02 -0400

    updated project name

---
+++ Tower-of-Hanoi/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CC_Proj_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.numDisc = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numDisc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 435);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // output
+            // 
+            this.output.AcceptsReturn = true;
+            this.output.Location = new System.Drawing.Point(47, 31);
+            this.output.Margin = new System.Windows.Forms.Padding(2);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output.Size = new System.Drawing.Size(320, 378);
+            this.output.TabIndex = 1;
+            // 
+            // numDisc
+            // 
+            this.numDisc.Location = new System.Drawing.Point(161, 445);
+            this.numDisc.Margin = new System.Windows.Forms.Padding(2);
+            this.numDisc.Name = "numDisc";
+            this.numDisc.Size = new System.Drawing.Size(68, 20);
+            this.numDisc.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 447);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of discs:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 506);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numDisc);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numDisc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.NumericUpDown numDisc;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tower-of-Hanoi/Form1.cs
@@ -0,0 +1,111 @@
+﻿//  Computational Complexity
+//  Florida Institute Of Technology
+//  Fall 2010
+//  Samuel Barlow
+
+using System;
+using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+    using System.Collections;
+
+namespace CC_Proj_1
+{
+    public partial class Form1 : Form
+    {
+        int moves = 0;
+        int times = 0;
+        string outputS = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// Tower af Hanoi
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Initialize variables
+            int temp;
+            moves = 0;
+            times = 0;
+            outputS = "";
+
+            
+            // Get the number of discs
+            int disc = (int)numDisc.Value;
+            
+            // Initialize the stacks
+            Stack<int> s1 = new Stack<int>(disc);
+            Stack<int> s2 = new Stack<int>(disc);
+            Stack<int> s3 = new Stack<int>(disc);
+            s1.Push(0);
+            s2.Push(0);
+            s3.Push(0);
+
+            // Populate the first stack
+            for(int x = disc; x > 0; x--)
+            {
+                s1.Push(x);
+            }
+
+            // Start the clock
+            DateTime startTime = DateTime.Now;
+
+            // Call recursive algorithm to solve tower
+            move(s1, s3, s2, '1', '3', '2', disc);
+            
+            // Stop the clock
+            DateTime stopTime = DateTime.Now;
+            TimeSpan duration = stopTime - startTime;
+
+
+            // Output Results
+            output.Text += outputS;
+            output.Text += "\r\n\r\nResults:\r\n";
+            output.Text += "Total moves = " + moves;
+            output.Text += "\r\nrecursion calls : " + times;
+            output.Text += "\r\nTotal time = " + duration.Minutes + " minutes " + duration.Seconds + " seconds " + duration.Milliseconds + " milliseconds ";
+            output.Text += "\r\nColumn 3:" ;
+            for (int x = 0; x < disc; x++)
+            {
+                temp= s3.Pop();
+                output.Text += "\r\n" + temp;
+            }
+            
+            
+        } // end
+
+        // **************************************************************************
+        // Recursive algorithm, to solve hanoi puzzle                               *
+        // Algorithm modified from: http://www.dreamincode.net/code/snippet474.htm  *
+        // **************************************************************************
+        void move(Stack<int> s1, Stack<int> s3, Stack<int> s2, char a, char c, char b, int disc) //, int last)
+        {
+            int temp;
+            times++;
+            if (disc > 0)
+            {  
+                move(s1, s2, s3, a, b, c, disc-1);
+                outputS = outputS + "\r\r\n Move " + disc + " from " + a + " to " + c;
+                moves++;
+                temp = s1.Pop();
+                s3.Push(temp);
+                move(s2, s3, s1, b, c, a, disc-1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
